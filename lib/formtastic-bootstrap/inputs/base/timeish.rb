e653c44e7b254bb20f0a2918601c52ddb42efb31
Romain Champourlier <pro@rchampourlier.com>
2012-06-07 12:00:53 +0200

    Added the date-select input and modified base/timeish to suit the changes from Formtastic 2.2.1

--- lib/formtastic-bootstrap/inputs/base/timeish.rb
+++ lib/formtastic-bootstrap/inputs/base/timeish.rb
@@ -3,6 +3,27 @@ module FormtasticBootstrap
     module Base
       module Timeish
 
+        def to_html
+          generic_input_wrapping do
+            fragments_wrapping do
+              hidden_fragments <<
+              template.content_tag(:div,
+                fragments.map do |fragment|
+                  fragment_wrapping do
+                    fragment_label_html(fragment) <<
+                    fragment_input_html(fragment)
+                  end
+                end.join.html_safe, # TODO is this safe?
+                { :class => 'fragments-group' } # TODO refactor to fragments_group_wrapping
+              )
+            end
+          end
+        end
+
+        def fragment_wrapping(&block)
+          template.content_tag(:div, template.capture(&block), fragment_wrapping_html_options)
+        end
+
         def date_input_html
           fragment_input_html(:date, "small")
         end
@@ -17,9 +38,14 @@ module FormtasticBootstrap
           "#{input_html_options[:id]}[#{fragment}]"
         end
 
-        def fragment_input_html(fragment, klass)
+        def fragment_label_html(fragment)
+          text = fragment_label(fragment)
+          text.blank? ? "".html_safe : template.content_tag(:label, text, :for => fragment_id(fragment))
+        end
+        
+        def fragment_input_html(fragment)
           opts = input_options.merge(:prefix => fragment_prefix, :field_name => fragment_name(fragment), :default => value, :include_blank => include_blank?)
-          template.send(:"text_field_#{fragment}", value, opts, input_html_options.merge(:id => fragment_id(fragment), :class => klass))
+          template.send(:"select_#{fragment}", value, opts, input_html_options.merge(:id => fragment_id(fragment)))
         end
 
       end
